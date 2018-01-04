ce76fe09118dc1b4ce1802d8b4a144a675d816e1
David Cautin <dacautin@unap.cl>
2018-01-04 16:52:59 -0300

    add the language select on the top bar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	// Starting animations
 	$("#top-banner").css('transform', 'translate(0px, 0px)');
 	var time = 400;
 	$(".card").each(function() {
@@ -7,3 +8,52 @@ $(document).ready(function() {
       	time += 400;
 	});
 });
+
+// Ready translated locale messages
+const messages = {
+  en: {
+  	languageName: 'English',
+  	jobRole: 'Web Developer',
+    myShortDescription: 'My name is David Cautin. I currently work at the IT department of <a href="http://www.unap.cl/" target="_blank">Arturo Prat University</a> as part of the software development team. I work as frontend web developer, role in which i have been able to affect the experience of hundreds of students and employees of the university.'    
+  },
+  es: {
+  	languageName: 'Español',
+  	jobRole: 'Desarrollador Web',
+    myShortDescription: 'Mi nombre es David Cautin. Actualmente trabajo en la unidad de informática de la <a href="http://www.unap.cl/" target="_blank">Universidad Arturo Prat</a>, como parte del equipo de desarrollo de software. Trabajo como desarrollador web, rol en el que he podido beneficiar la experiencia de cientos de estudiantes y empleados de la universidad.'    
+  }
+}
+
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+  locale: 'en', // set default locale
+  messages, // set locale messages
+})
+
+var localesList = [];
+for(var propertyName in messages) {
+  // propertyName is what you want
+  // you can get the value like this: myObject[propertyName]
+  //console.log(propertyName);
+  //console.log(messages[propertyName].language);
+  localesList.push({
+    code: propertyName,
+    languageName: messages[propertyName].languageName
+  });
+}
+
+// Create a Vue instance with `i18n` option
+new Vue({
+  i18n,
+  data: {
+    selected: i18n.locale,
+    locales: localesList
+  }
+}).$mount('#app')
+
+// Now the app has started!
+
+function updateLocale(){
+  i18n.locale = document.getElementById('selLocale').value;
+  //console.log(document.getElementById('selLocale').value);
+  //console.log(i18n.locale);
+}
