a093458529c21a2d6d7c0f5c2d2a22c34f0ee9ae
PinguinoSod <pinguino.sod@gmail.com>
2018-01-12 20:49:19 -0300

    Redesign, new colors, new logo, etc

--- js/main.js
+++ js/main.js
@@ -21,17 +21,14 @@ $(document).ready(function() {
 const messages = {
   en: {
   	languageName: 'English',
-  	jobRole: 'Web Developer',
     myShortDescription: 'My name is David Cautin. I currently work at the IT department of <a href="http://www.unap.cl/" target="_blank">Arturo Prat University</a> as part of the software development team. I work as frontend web developer, role in which i have been able to affect the experience of hundreds of students and employees of the university.'
   },
   es: {
   	languageName: 'Español',
-  	jobRole: 'Desarrollador Web',
     myShortDescription: 'Mi nombre es David Cautin. Actualmente trabajo en la unidad de informática de la <a href="http://www.unap.cl/" target="_blank">Universidad Arturo Prat</a>, como parte del equipo de desarrollo de software. Trabajo como desarrollador web, rol en el que he podido beneficiar la experiencia de cientos de estudiantes y empleados de la universidad.'
   },
   it: {
   	languageName: 'Italiano',
-  	jobRole: 'Sviluppatore Web',
   	myShortDescription: 'Mi chiamo David Cautin. Attualmente lavoro presso il dipartimento IT della <a href="http://www.unap.cl/" target="_blank">Università Arturo Prat</a> come parte del team di sviluppo del software. Lavoro come sviluppatore web frontend, ruolo in cui sono stato in grado di influenzare l\'esperienza di centinaia di studenti e dipendenti dell\'università.'
   }
   /*de: {
